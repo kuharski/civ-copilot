cce139b0808219a8896e1fe9d81cf50811576930
swagmonster <98782228+kuharski@users.noreply.github.com>
2025-05-31 20:35:04 -0400

    selected LLM model

--- data/ingest/stratEnrichment.js
+++ data/ingest/stratEnrichment.js
@@ -1,24 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
 import Civilization from "../../models/Civilization.js";
 import Unit from "../../models/Unit.js";
 import Building from "../../models/Building.js"
 
+import OpenAI from "openai";
+const client = new OpenAI(
+    base_url = "https://api.fireworks.ai/inference/v1",
+    api_key = process.env.AI_API_KEY,
+);
 
 export default async function enrichStrats() {
 
     try {
         const allCivs = await Civilization.find({});
 
-        for(const doc of allCivs) {
+        for (const doc of allCivs) {
             console.log("Name: " + doc.civ.name);
-            for(const unit of doc.civ.uniqueUnits) {
-                const unitObj = await Unit.findOne({name: unit});
-                if(unitObj) {
+            for (const unit of doc.civ.uniqueUnits) {
+                const unitObj = await Unit.findOne({ name: unit });
+                if (unitObj) {
                     console.log(unit + " : " + unitObj.info);
                 }
             }
-            for(const building of doc.civ.uniqueBuildings) {
-                const buildingObj = await Building.findOne({name: building});
-                if(buildingObj) {
+            for (const building of doc.civ.uniqueBuildings) {
+                const buildingObj = await Building.findOne({ name: building });
+                if (buildingObj) {
                     console.log(building + " : " + buildingObj.info);
                 }
             }
@@ -26,7 +33,7 @@ export default async function enrichStrats() {
             console.log(" ");
         }
 
-    } catch(err) {
+    } catch (err) {
         console.error("error in enrichStrats: ", err);
     }
 }
