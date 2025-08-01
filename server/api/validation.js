96c66f24ddfea49a6f63cf01e85802e4f7fdd871
Andrew Kuharski <98782228+kuharski@users.noreply.github.com>
2025-07-31 23:08:50 -0400

    tightened up input validation

--- server/api/validation.js
+++ server/api/validation.js
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 const techsRequestSchema = z.object({
-    leader: z.string(),
-    playerScenario: z.string().max(1000),
-    techs: z.array(z.string()).optional()
+    leader: z.string().max(40),
+    playerScenario: z.string().max(750),
+    techs: z.array(z.string()).max(81).optional()
 });
 
 export default techsRequestSchema;
